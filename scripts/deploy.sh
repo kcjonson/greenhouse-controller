4a91073720d7cead55ddf94d78b131fd61124a13
Kevin Jonson <kevin.jonson@redfin.com>
2018-07-12 16:48:25 -0700

    fixing yarn lockfile ... again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,3 +2,4 @@ source .env
 
 rsync -r --delete-after --quiet --filter=':- .gitignore' . ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_LOCATION}
 
+ssh ${DEPLOY_USER}@${DEPLOY_HOST} 'cd ${DEPLOY_LOCATION} && npm run start'
