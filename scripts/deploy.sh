9d156b1037c06c481fbbace66c6847408723d0c2
Kevin Jonson <kevin.jonson@redfin.com>
2018-07-12 17:50:46 -0700

    attempting to fix deploy, still

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
-source .env
-
 rsync -r --delete-after --quiet --filter=':- .gitignore' . ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_LOCATION}
 
 ssh ${DEPLOY_USER}@${DEPLOY_HOST} 'cd ${DEPLOY_LOCATION} && npm run start'
